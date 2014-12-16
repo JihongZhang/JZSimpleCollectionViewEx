409dc4618008f74b10c6fdaf01735c5d7328c2c5
Jihong Zhang <z_jihong@yahoo.com>
2014-12-15 18:08:50 -0800

    first commit

---
+++ AppDelegate.swift
@@ -0,0 +1,68 @@
+//
+//  AppDelegate.swift
+//  JZSimpleCollectionViewEx
+//
+//  Created by Jihong Zhang on 12/15/14.
+//  Copyright (c) 2014 Jihong Zhang. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        if let window = window {
+            
+            //let vc1:UIViewController = ViewController()
+            let vc1:UIViewController = MyCollectionViewController()
+            let nav1 = UINavigationController(rootViewController: vc1);
+            
+            let image1 = UIImage(named:"icon_shop");
+            nav1.tabBarItem = UITabBarItem(title: "SHOP", image:image1, tag: 1);
+            
+            
+            
+            let arr = [nav1 ];
+            let tabBarController = UITabBarController();
+            tabBarController.viewControllers = arr;
+            self.window!.rootViewController = tabBarController;
+            
+            
+            window.backgroundColor = UIColor.whiteColor()
+            window.makeKeyAndVisible()
+            //window.rootViewController = nav1
+        }
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
---
+++ ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  JZSimpleCollectionViewEx
+//
+//  Created by Jihong Zhang on 12/15/14.
+//  Copyright (c) 2014 Jihong Zhang. All rights reserved.
+//
+
+import UIKit
+
+let HorizontalSpacing : CGFloat = 5.0
+let VerticalSpacing : CGFloat = 5.0
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+ 
+         
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
